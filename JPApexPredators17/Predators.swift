95681c4d58868cfbdd39847b83a0b29c5c6e47eb
Daniel <dannyblaze469@gmail.com>
2024-03-17 15:23:21 -0700

    Created JPApexPredator Project

---
+++ JPApexPredators17/Predators.swift
@@ -0,0 +1,18 @@
+//
+//  Predators.swift
+//  JPApexPredators17
+//
+//  Created by Daniel Ezekiel on 3/17/24.
+//
+
+import Foundation
+
+class Predators{
+    var apexPredators: [ApexPredator] = []
+    
+    func decodeApexPredatorData() {
+        if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json") {
+            
+        }
+    }
+}
