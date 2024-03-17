4e54225acdd5472cf0b811a0d390f69926051798
Daniel <dannyblaze469@gmail.com>
2024-03-17 16:26:23 -0700

    Decoded JSON file and added data to content view

--- JPApexPredators17/ContentView.swift
+++ JPApexPredators17/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let predators = Predators()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(predators.apexPredators) { predador in
+            HStack{
+                //Dinosaur image
+                Image(predador.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .shadow( color: . white, radius: 1)
+                
+                VStack(alignment: .leading) {
+                    // Name
+                    Text(predador.name)
+                        .fontWeight(.bold)
+                    
+                    // Type
+                    Text(predador.type.rawValue.capitalized)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 13)
+                        .padding(.vertical, 5)
+                        .background(predador.type.background)
+                        .clipShape(.capsule)
+                }
+            }
         }
-        .padding()
+        .preferredColorScheme(.dark)
     }
 }
 
