3d78d8c302d5da5abd9dc22fc8a3730c0cbca267
Daniel <dannyblaze469@gmail.com>
2024-03-20 22:06:54 -0700

    Done with main app

--- JPApexPredators17/PredatorMap.swift
+++ JPApexPredators17/PredatorMap.swift
@@ -12,15 +12,37 @@ struct PredatorMap: View {
     let predators = Predators()
     
     @State var position: MapCameraPosition
+    @State var satellite = false
     
     var body: some View {
         Map(position: $position){
             ForEach(predators.apexPredators){ predator in
                 Annotation(predator.name, coordinate: predator.location){
                     Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 100)
+                        .shadow(color: .white, radius: 3)
+                        .scaleEffect(x: -1)
                 }
             }
         }
+        .mapStyle(satellite ? .imagery(elevation: .realistic) : .standard(elevation: .realistic))
+        .overlay(alignment: .bottomTrailing){
+            Button {
+                satellite.toggle()
+            } label: {
+                Image(systemName: satellite ? "globe.americas.fill" : "globe.americas")
+                    .font(.largeTitle)
+                    .imageScale(.large)
+                    .padding(3)
+                    .background(.ultraThinMaterial)
+                    .clipShape(.rect(cornerRadius: 7))
+                    .shadow(radius: 3)
+                    .padding()
+            }
+        }
+        .toolbarBackground(.automatic)
     }
 }
 
